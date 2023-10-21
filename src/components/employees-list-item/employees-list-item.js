689d112c1c3c5626c840b0e9b8cfbd35859a25db
Denis Selitsky <denis.selitsky@gmail.com>
2023-10-21 19:16:55 +0300

    feat: add handle click on span with name

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,14 +1,58 @@
 import "./employees-list-item.css";
+import { Component } from "react";
 
-const EmployeesListItem = ({ name, salary, increase }) => {
-    const classNames = increase ? "list-group-item d-flex justify-content-between increase" :
-        "list-group-item d-flex justify-content-between";
+
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            promotion: false
+        };
+    }
+
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    };
+
+    onPromotion = () => {
+        this.setState(({promotion}) => ({
+            promotion: !promotion
+        })) 
+    };
+
+
+    // changeClasses = (nameState,classForChange) => {
+    //     // const {nameState} = this.state; 
+    //     if (nameState) {
+    //         spanClasses += ` ${classForChange}`;
+    //     }else {
+    //         spanClasses = spanClasses.split(' ').filter((el)=> {
+    //             return el === `${classForChange}`?false:true;
+    //         }).join(" ");
+    //     }
+    // } 
+
+    render() {
+        const {name,salary} = this.props;
+        const {increase, promotion} = this.state;
+
+        let classNames =  "list-group-item d-flex justify-content-between";
+        if(increase) {
+            classNames += ' increase';
+        }
+
+        if(promotion) {
+            classNames += ' like';
+        }
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span onClick={this.onPromotion} className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
             <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-cookie btn-sm">
+                <button onClick={this.onIncrease} className="btn-cookie btn-sm">
                     <i className="fas fa-cookie"></i>
                 </button>
                 <button className="btn-trash btn-sm">
@@ -18,6 +62,8 @@ const EmployeesListItem = ({ name, salary, increase }) => {
             </div>
         </li>
     );
+    }
+    
 };
 
 export default EmployeesListItem;
